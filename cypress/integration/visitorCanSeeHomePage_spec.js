6f5cf8912e1cc41b86b887b677cef7859d822e7a
noel <noelgerard.ryan@gmail.com>
2021-07-06 15:24:31 +0100

    Adding Basic test to see home page h1, Adding empty components to navbar and singlepost to test routes are working, Deleting react logo.svg

--- cypress/integration/visitorCanSeeHomePage_spec.js
+++ cypress/integration/visitorCanSeeHomePage_spec.js
@@ -1,5 +1,12 @@
 describe('vistor can see home page', () => {
-  it('test', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
+
+  it('has h1 Home Page', () => {
+    cy.contains('Home Page!')
+  })
+  
+
+
 })
--- src/App.js
+++ src/App.js
@@ -9,13 +9,12 @@ function App() {
   return(
     <BrowserRouter>
       <Switch>
-        <Route component={Home} path='/'/>
+        <Route component={Home} path='/' exact/>
         <Route component={About} path='/about'/>
         <Route component={SinglePost} path='/post/:slug'/>
         <Route component={Post} path='/post'/>
         <Route component={Project} path='/project'/>
       </Switch>
-
     </BrowserRouter>
   ) 
 }
