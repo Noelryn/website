fd567e32a3cb21ee8c9c40fce8ad7833e3160f60
noel <noelgerard.ryan@gmail.com>
2021-07-06 17:37:10 +0100

    Adding basic styling to the NavBar component to make it easier to view, Installing react-social-icons package using npm, adding SocialIcon tags to NavBar and styling to show social media Icons with active links in Navbar, Adding tests NavBar Links

--- cypress/integration/visitorCanSeeHomePage_spec.js
+++ cypress/integration/visitorCanSeeHomePage_spec.js
@@ -3,10 +3,22 @@ describe('vistor can see home page', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('has h1 Home Page', () => {
+  it('user can see Nav Bar', () => {
     cy.contains('Home Page!')
-    cy.contains('HomeAboutPostProjects')
+    cy.contains('About') .click() 
+    cy.contains('About Page!')
+    cy.contains('Post') .click() 
+    cy.contains('Post Page!')
+    cy.contains('Projects') .click() 
+    cy.contains('Project Page!')
   })
+
+  it('has social media icon links', () => {
+    cy.contains('linkedin-icon').click()
+    cy.contains('Noel Ryan')
+  })
+
+
 
   
   
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,31 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
+import { SocialIcon } from "react-social-icons"
 
 export default function NavBar() {
   return (
-    <header>
-      <div>
-        <nav>
-          <NavLink to="/" exact>
+    <header className="bg-blue-400">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink to="/" exact activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-4 text-white hover:text-blue-800 text-4xl font-bold cursive tracking-widest">
             Home
           </NavLink>
-          <NavLink to="/about">
+          <NavLink to="/about" activeClassName="text-white bg-blue-900" className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-blue-800">
             About
           </NavLink>
-          <NavLink to="/post">
+          <NavLink to="/post" activeClassName="text-white bg-blue-900" className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-blue-800">
             Post
           </NavLink>
-          <NavLink to="/project">
+          <NavLink to="/project" activeClassName="ttext-white bg-blue-900" className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-blue-800">
             Projects
           </NavLink>
         </nav>
+        <div className=" inline-flex py-3 px-3 my-4">
+          <SocialIcon url="https://www.linkedin.com/in/noel-ryan-3934b773/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+          <SocialIcon url="https://github.com/Noelryn" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+          <SocialIcon url="https://twitter.com/NoelRyan5" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
+        </div>
+        
       </div>
     </header>
   )
